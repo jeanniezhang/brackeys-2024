4d9270f4348b06e7367659870013a558613930c8
jeanniezhang <86752929+jeanniezhang@users.noreply.github.com>
2024-02-12 10:45:18 -0500

    add game manager and audio systems file

---
+++ Brackey 2024/Assets/Scripts/AudioSystem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSystem : MonoBehaviour
+{
+    public string ReturnAudio() {
+        return "Audio playing";
+    }
+}
---
+++ Brackey 2024/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    // globally accessed class to manage game state
+    public static GameManager instance;
+    public GameState state;
+    public static event Action<GameState> onGameStateChanged;
+    private AudioSystem audioSystem;
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        else {
+            Destroy(gameObject);
+        }
+        
+        audioSystem = GetComponent<AudioSystem>();
+    }
+
+    void Start() {
+        UpdateGameState(GameState.MenuSelect);
+    }
+
+    public void UpdateGameState(GameState newState) {
+        state = newState;
+        // handles state specific logic
+        switch(newState) {
+            case GameState.MenuSelect:
+                break;
+            case GameState.Playing:
+                break;
+            case GameState.GameOver:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+        onGameStateChanged?.Invoke(newState);
+    }
+
+    public void playAudio() {
+        Debug.Log(audioSystem.ReturnAudio());
+    }
+}
+
+public enum GameState {
+    MenuSelect,
+    Playing,
+    GameOver
+}
