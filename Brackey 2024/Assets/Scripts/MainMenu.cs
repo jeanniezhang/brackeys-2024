96013e673839a09fafe228cbff1289ed31a4ce8e
jeanniezhang <86752929+jeanniezhang@users.noreply.github.com>
2024-02-17 14:43:59 -0500

    add sprites and menu event system

--- Brackey 2024/Assets/Scripts/MainMenu.cs
+++ Brackey 2024/Assets/Scripts/MainMenu.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
-    public void GoToScene(string sceneName) {
+    IEnumerator waiter(string sceneName)
+    {
+        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene(sceneName);
+        
+    }
+    public void GoToScene(string sceneName) {
+        StartCoroutine(waiter(sceneName));
+        // SceneManager.LoadScene(sceneName);
+        // WaitForSeconds(1);
     }
 
     public void QuitApp() {
